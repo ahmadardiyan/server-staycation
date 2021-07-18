97548928036ad88320e52d751849804b091faa8e
ahmadardiyan <ahmadardiyanto23@gmail.com>
2021-07-18 10:44:55 +0700

    create flash message/alert

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 // import and connect to mongoose
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/db_bwamern',{
+mongoose.connect('mongodb://localhost:27017/db_bwamern', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -26,28 +28,41 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(methodOverride('_method'));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 60000
+  }
+}))
+app.use(flash());
+
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/sb-admin-2', express.static(path.join(__dirname,'node_modules/startbootstrap-sb-admin-2')));
+app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // admin
-app.use('/admin',adminRouter);
+app.use('/admin', adminRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,26 +2,42 @@ const Category = require('../models/Category');
 
 module.exports = {
 	viewDashboard: (req, res) => {
-		res.render('admin/dashboard/view_dashboard');
+		res.render('admin/dashboard/view_dashboard', {
+			title : 'StayCation | Dashboard'
+		});
 	},
 	viewCategory: async (req, res) => {
-		const category = await Category.find();
-
-		res.render('admin/category/view_category', {
-			category
-		});
+		try {
+			const category = await Category.find();
+			const alertMessage = req.flash('alertMessage');
+			const alertStatus = req.flash('alertStatus');
+			const alert = {
+				message : alertMessage,
+				status : alertStatus
+			}
+			res.render('admin/category/view_category', {
+				category,
+				alert,
+				title : 'StayCation | Category'
+			});
+		} catch(error) {
+			res.render('admin/category');
+		}
 	},
 	createCategory: async (req, res) => {
 		try {
-			let {
-				name
-			} = req.body;
+			let { name } = req.body;
 
-			await Category.create({
-				name
-			});
+			req.flash('alertMessage', 'Success create data category!' );
+			req.flash('alertStatus', 'success' );
+
+			await Category.create({ name });
 			res.redirect('/admin/category');
-		} catch {
+		} catch(error) {
+
+			req.flash('alertMessage', `$error.message` );
+			req.flash('alertStatus', 'danger' );
+
 			res.redirect('/admin/category');
 		}
 	},
@@ -37,28 +53,53 @@ module.exports = {
 
 			category.name = name;
 			await category.save();
+
+			req.flash('alertMessage', 'Success updated data category!' );
+			req.flash('alertStatus', 'success' );
+
 			res.redirect('/admin/category');
-		} catch {
+		} catch(error) {
+
+			req.flash('alertMessage', `$error.message` );
+			req.flash('alertStatus', 'danger' );
+
 			res.redirect('/admin/category');
 		}
 	},
 	deleteCategory: async (req, res) => {
-		const {
-			id
-		} = req.params;
-		const category = await Category.findOne({
-			_id: id
-		});
-		await category.remove();
-		res.redirect('/admin/category');
+		try {
+			const {
+				id
+			} = req.params;
+			const category = await Category.findOne({
+				_id: id
+			});
+			await category.remove();
+
+			req.flash('alertMessage', 'Success deleted data category!' );
+			req.flash('alertStatus', 'success' );
+
+			res.redirect('/admin/category');
+		} catch(error) {
+			req.flash('alertMessage', `$error.message` );
+			req.flash('alertStatus', 'danger' );
+
+			res.redirect('/admin/category');
+		}
 	},
 	viewBank: (req, res) => {
-		res.render('admin/bank/view_bank');
+		res.render('admin/bank/view_bank',{
+			title : 'StayCation | Bank'
+		});
 	},
 	viewItem: (req, res) => {
-		res.render('admin/item/view_item');
+		res.render('admin/item/view_item', {
+			title : 'StayCation | Item'
+		});
 	},
 	viewBooking: (req, res) => {
-		res.render('admin/booking/view_booking');
+		res.render('admin/booking/view_booking',{
+			title : 'StayCation | Booking'
+		});
 	}
 }
