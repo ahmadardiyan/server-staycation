96d86276a4752a13fa3e25d29c141f31a60537f5
ahmadardiyan <ahmadardiyanto23@gmail.com>
2021-10-20 21:48:11 +0700

    funcntion delete bank

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -167,6 +167,25 @@ module.exports = {
 			res.redirect('/admin/bank');
 		}
 	},
+	deleteBank: async (req, res) => {
+		try {
+			const {id}  = req.params;
+			const bank = await Bank.findOne({_id: id});
+			fs.unlink(path.join(`public/${bank.imageUrl}`));
+
+			await bank.remove();
+
+			req.flash('alertMessage', 'Success updated data bank!' );
+			req.flash('alertStatus', 'success' );
+
+			res.redirect('/admin/bank');
+		} catch (error) {
+			req.flash('alertMessage', `${error.message}` );
+			req.flash('alertStatus', 'danger' );
+
+			res.redirect('/admin/bank');
+		}
+	},
 	viewItem: (req, res) => {
 		res.render('admin/item/view_item', {
 			title : 'StayCation | Item'
--- routes/admin.js
+++ routes/admin.js
@@ -14,6 +14,7 @@ router.delete('/category/:id',adminController.deleteCategory);
 router.get('/bank',adminController.viewBank);
 router.post('/bank', upload, adminController.createBank);
 router.put('/bank', upload, adminController.updateBank);
+router.delete('/bank/:id',adminController.deleteBank);
 
 router.get('/item',adminController.viewItem);
 router.get('/booking',adminController.viewBooking);
